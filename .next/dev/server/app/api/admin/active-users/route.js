71ac4e38b6008627edcdc761a73053d594741b14
Preetham Praveen <preethampraveencode26@gmail.com>
2026-01-28 16:18:59 +0530

    fix: remove .next from git and update .gitignore

--- .next/dev/server/app/api/admin/active-users/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/admin/active-users/route.js")
-R.c("server/chunks/[root-of-the-server]__d59695ec._.js")
-R.c("server/chunks/[root-of-the-server]__48df87e3._.js")
-R.c("server/chunks/_next-internal_server_app_api_admin_active-users_route_actions_0a827522.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/admin/active-users/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/admin/active-users/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/chunks/_next-internal_server_app_api_admin_buses_route_actions_a3561c45.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/admin/buses/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_admin_buses_route_actions_a3561c45.js.map
